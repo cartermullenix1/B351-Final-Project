d5935a53fa719ce4ba6f2143f564da96af5214c0
Tri Nguyen <81249845+tringuyen180303@users.noreply.github.com>
2024-12-27 18:15:02 -0500

    fixed files

--- db/session.py
+++ db/session.py
@@ -6,10 +6,5 @@ SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL is", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
 
